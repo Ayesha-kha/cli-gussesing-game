6823838501d3fb516cb9d04f5e5a98b966ad3cad
Ayesha-kha <ayeshkhan.82003@gmail.com>
2024-03-26 12:07:48 +0500

    created number gussesing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#!/ user/bin/env node
+
+import inquirer from "inquirer";
+// guessing the number
+const randomNumber = Math.floor(Math.random()* 20 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name:"userGuessNumber",
+        type:"number",
+        message:"Please guess a number between 1 to 20:",
+    },
+]);
+
+if(answer.userGuessNumber === randomNumber){
+    console.log("Your number is correct and you won the game")
+}else{
+    console.log("Sorry! you guess wrong number try again");
+}
---
+++ node_modules/@ayekh8/cli-gussesing-game/README.md
@@ -0,0 +1,2 @@
+# cli-gussesing-game
+Today we are working on the number guessing game.
